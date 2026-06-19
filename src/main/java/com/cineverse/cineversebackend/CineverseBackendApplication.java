2a35a02c44fe0fa7a9ca271249a99526afde56fb
rohitchawdhari <rohitchawdhari48@gmail.com>
2026-06-19 19:06:10 +0530

    Debug Railway env vars

--- src/main/java/com/cineverse/cineversebackend/CineverseBackendApplication.java
+++ src/main/java/com/cineverse/cineversebackend/CineverseBackendApplication.java
@@ -6,8 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CineverseBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CineverseBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
 
+        System.out.println("=================================");
+        System.out.println("MONGO URI = " + System.getenv("SPRING_DATA_MONGODB_URI"));
+        System.out.println("PORT = " + System.getenv("PORT"));
+        System.out.println("=================================");
+
+        SpringApplication.run(CineverseBackendApplication.class, args);
+    }
 }
